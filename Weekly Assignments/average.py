df04846c74ed427dde8899e8262bf251c0082441
Ramirej5 <Ramirej5@oregonstate.edu>
2022-04-08 21:38:16 -0700

    average

---
+++ Weekly Assignments/average.py
@@ -0,0 +1,15 @@
+# Name: Jose Ramirez
+# ENGR 103
+# Project 2a
+
+print("Please enter five numbers. ")
+
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+
+avg1 = float(num1+num2+num3+num4+num5)/5
+print("The average of those numbers is: ")
+print(float(avg1))
