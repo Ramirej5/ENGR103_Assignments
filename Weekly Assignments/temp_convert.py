7d69b6781377fb699b56455e06e7e1f3d621cd74
Ramirej5 <Ramirej5@oregonstate.edu>
2022-04-08 21:38:29 -0700

    temp

---
+++ Weekly Assignments/temp_convert.py
@@ -0,0 +1,11 @@
+# Jose Ramirez
+# ENGR 103
+# Assignment 2.b
+
+print("Please enter a Celsius temperature. ")
+
+temp = float(input())
+F = (9/5)*temp+32
+
+print("The equivalent Fahrenheit temperature is: ")
+print(F)
